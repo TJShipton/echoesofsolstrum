c05b568f21ca4a2dbd6865d711fe80b68aaa10a3
Christian <cjwinkler0@gmail.com>
2023-11-02 16:15:54 -0400

    jump anims

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -98,6 +98,11 @@ public class PlayerController : MonoBehaviour, IDamageable
 
         // Jump
         isGrounded = IsGrounded();  // Update the isGrounded variable
+        animator.SetBool("IsGrounded", isGrounded);
+        if (isGrounded)
+        {
+            animator.SetBool("IsFalling", false);
+        }
 
         if (shouldJump)  // Check if jump input was received
         {
@@ -116,6 +121,12 @@ public class PlayerController : MonoBehaviour, IDamageable
             canDoubleJump = false;  // Disable further double jumps until grounded again
         }
 
+        // Check for falling
+
+        if(rb.velocity.y < -0.1f)  // Assumes that if the velocity is less than a small negative value, the character is falling
+        {
+            animator.SetBool("IsFalling", true);
+        }
 
     }
     private bool IsGrounded()
@@ -148,10 +159,13 @@ public class PlayerController : MonoBehaviour, IDamageable
             if (isGrounded)  // Only jump if grounded and input just started
             {
                 shouldJump = true;  // Set flag to true when jump input is received
+                animator.SetTrigger("JumpTrigger");
+
             }
             else if (canDoubleJump)  // Allow double jump if not grounded but double jump is allowed
             {
                 shouldDoubleJump = true;  // Set flag to true when double jump input is received
+                animator.SetTrigger("DoubleJumpTrigger");
                 Debug.Log("Double jump input received.");  // Log when double jump input is received
             }
         }
