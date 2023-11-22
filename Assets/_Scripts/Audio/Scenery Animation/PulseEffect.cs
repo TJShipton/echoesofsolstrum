cde0d20722272bb7a9b97b2ff81bb42ba9a1911a
TJShipton <tjshipton@live.com>
2023-11-22 12:26:25 +0000

    Mushroom tests

--- Assets/_Scripts/Audio/Scenery Animation/PulseEffect.cs
+++ Assets/_Scripts/Audio/Scenery Animation/PulseEffect.cs
@@ -3,23 +3,42 @@ using UnityEngine;
 
 public class PulseEffect : MonoBehaviour
 {
-    public Animation animCom;
+    public float maxScale = 1.5f; // Maximum scale of the pulse
+    public float pulseDuration = 1f; // Duration of each pulse
+
+    private Vector3 originalScale; // Original scale of the object
+    private float pulseTimer = 0f; // Timer to track the pulse progress
 
     [EventID]
-    public string bassDrumEventID = "BassDrum";
+    public string pulseEventID = "Pulse";
 
     void Awake()
     {
-        Koreographer.Instance.RegisterForEvents(bassDrumEventID, OnAnimationTrigger);
-
+        originalScale = transform.localScale;
+        Koreographer.Instance.RegisterForEvents(pulseEventID, OnPulseTrigger);
     }
 
-    void OnAnimationTrigger(KoreographyEvent evt)
+    void Update()
     {
-        animCom.Stop();
-        animCom.Play();
+        if (pulseTimer > 0)
+        {
+            // Decrease the timer
+            pulseTimer -= Time.deltaTime;
+
+            // Calculate the scale factor
+            float scale = originalScale.x * (1 + Mathf.Sin((1 - (pulseTimer / pulseDuration)) * Mathf.PI) * (maxScale - 1));
+            transform.localScale = new Vector3(scale, scale, scale);
+        }
+        else
+        {
+            // Reset to original scale when not pulsing
+            transform.localScale = originalScale;
+        }
     }
 
-
+    void OnPulseTrigger(KoreographyEvent evt)
+    {
+        // Reset the pulse timer
+        pulseTimer = pulseDuration;
+    }
 }
-
